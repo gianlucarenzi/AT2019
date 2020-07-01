e8e4b418408b8c71c851b7231a901e40daf40057
Gianluca Renzi <gianlucarenzi@eurek.it>
2020-07-01 10:46:22 +0200

    Rewritten. Added a clearer way to setup portb and added a "fake" c64 loading module... ;-)

--- ATARI-Driver/megaram.c
+++ ATARI-Driver/megaram.c
@@ -1,7 +1,6 @@
-/*
- * C64 Decrunching simulator. It's a FAKE!!! ;-)
- * OS ROM RAM Enabler
-*/
+//
+// OS ROM RAM Expansion Checker
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,29 +16,23 @@
 
 #define PLP()	__asm__("plp")
 #define PHP()	__asm__("php")
+
 #define ArraySize(a)	(sizeof(a)/sizeof(a[0]))
-//#define POKE(addr,val)     (*(unsigned char*) (addr) = (val))
-//#define PEEK(addr)         (*(unsigned char*) (addr))
 #define peek(a)		PEEK(a)
 #define poke(a,b)	POKE(a,b)
 
-#define KB(a)	((a)*1024L)
-#define MB(a)	(KB(a)*1024L)
-#ifndef MEMSIZEK
-	#error "MEMSIZEK must be defined"
-#else
-	#define MEMORY_SIZE KB(MEMSIZEK)
-#endif
+static int nmien_reg = 0;
+static unsigned char * nmien = (unsigned char *) 0xD40E;
+#define CRAZY_COLOR		poke((unsigned char *) 0xD01A, rand());
 
 #define ENTER_CRITICAL() \
-	PHP(); \
 	SEI(); \
 	nmien_reg = peek(nmien); \
 	poke(nmien, 0);
 
 #define EXIT_CRITICAL() \
 	poke(nmien, nmien_reg); \
-	PLP();
+	CLI();
 
 static int mode_576k[32] = {
 	0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E,
@@ -73,14 +66,12 @@ static void delay(int l)
 		asm_delay(255);
 }
 
-#define CRAZY_COLOR		poke((unsigned char *) 0xD01A, rand());
-
 int main(void)
 {
 	int reg;
-	int m_portb;
+	int m_portb, dmareg;
 	char c;
-	unsigned char * wram = (unsigned char *) 0x7FFF; // 0x4000-0x7FFF
+	unsigned char * wram = (unsigned char *) 0x5300; // RAM window under ROM SELF TEST
 	unsigned char * osram = (unsigned char *) 0x3000;
 	unsigned char * osrom;
 	unsigned char * portb = (unsigned char *) 0xD301;
@@ -89,8 +80,17 @@ int main(void)
 	int i;
 	int bad_main = 0;
 	int bad_expansion = 0;
+	unsigned char fake[] = {
+		'L', 'O', 'A', 'D', '"', '$', '"', ',', '8', ',', '1', 0x9b, 0x9b,
+		'S', 'E', 'A', 'R', 'C', 'H', 'I', 'N', 'G', ' ', 'F', 'O', 'R', ' ', '$', 0x9b,
+		'L', 'O', 'A', 'D', 'I', 'N', 'G', 0x9b,
+		'\0'// must be last!
+	};
+	unsigned char * ptr;
 
-	m_portb = peek(559);
+	dmareg = peek(559);
+	m_portb = peek(portb);
+
 	poke(559, 0);
 	srand(0xdeadbeef);
 	clrscr();
@@ -98,8 +98,11 @@ int main(void)
 #error "Use target as atarixl"
 #endif
 
-	/* Commodore 64 Chargen ROM */
-	
+	// This program must be compiled and linked with the cc65 unique feature
+	// of startup C code for mode: atarixl. It change the OS from
+	// ROM to RAM!!
+
+	// Commodore 64 Chargen ROM */
 	// chars from 32 to 63 '@ABC...[\]^_
 	osrom = (unsigned char *) (0xE000 + ('@' - 32) * 8);
 	count = 32;
@@ -123,59 +126,90 @@ int main(void)
 	printf("\n  **** ATARI 130XE MEMORY TESTER ****\n\n");
 	printf(" 576K INTERNAL EXPANSION RAM SYSTEM BY\n         Scott Peterson (c) 1986\n");
 	printf("   written by Gianluca Renzi (c) 2020\n\n");
+	printf("READY.\n");
+	poke(559, dmareg);
+
+	ptr = fake;
+	for (i = 0; i < 120; i++)
+	{
+		if (*(ptr) == '\0')
+			break;
+		putchar(*(ptr));
+		ptr++;
+		delay(1);
+	}
 	printf("READY.\n%c\n\n", 128 + ' ');
-	poke(559, m_portb);
-	delay(150);
+	delay(50);
 
 #ifdef M192K
 	count = 0;
-	/* 130XE mode: 192K RAM Pia Port B bank selection bit 2,3,6 */
+	// 130XE mode: 192K RAM Pia Port B bank selection bit 2,3,6 (and bit 4??)
 	printf("Checking 130XE Mode: ");
+	ENTER_CRITICAL();
 	for (i = 0; i < 8; i++)
 	{
 		poke(portb, 0x72); // https://www.atarimagazines.com/v4n7/memorymanagement.html
-		/*
-		 * 7 6 5 4 3 2 1 0 
-		 * 0 1 1 1 0 0 1 0 == 0x72
-		 * 
-		 * CPU & ANTIC accessing Main Memory space (NO EXPANSION WINDOW)
-		 */
+		//
+		// 7 6 5 4 3 2 1 0 
+		// 0 1 1 1 0 0 1 0 == 0x72
+		// 
+		// CPU & ANTIC accessing Main Memory space (NO EXPANSION WINDOW)
+		//
 		poke(wram, 0xAA); // Watermark @ main bank 0x4000
 		CRAZY_COLOR;
-		
-		/* Now accessing @ bank # (CPU & ANTIC) */
-		poke(portb, 128 + mode_130xe[i] + 2);
-		poke(wram, bank_val[i]);    // Write i @ bank # @ 0x4000
-		CRAZY_COLOR;
 
-		// read back the value
+		// Now accessing @ bank # (CPU & ANTIC)
+		reg = 128 + mode_130xe[i];			// Select BANK #
+		reg = reg | (1 << 1);				// Basic OFF
+		reg = reg & 0xFE;					// OS ROM Ram space
+		poke(portb, reg);
+		poke(wram, bank_val[i]);			// Write i @ bank # @
+		CRAZY_COLOR;
+	}
+
+	for (i = 0; i < 8; i++)
+	{
+		// Now read back the values
+		poke(portb, 0x72); // https://www.atarimagazines.com/v4n7/memorymanagement.html
+		//
+		// 7 6 5 4 3 2 1 0 
+		// 0 1 1 1 0 0 1 0 == 0x72
+		// 
+		// CPU & ANTIC accessing Main Memory space (NO EXPANSION WINDOW)
+		//
 		reg = peek(wram);
+		CRAZY_COLOR;
 		// Test if it is correctly written/read
-		if (reg != bank_val[ i ])
+		if (reg != 0xAA)
 		{
-			//printf("ERROR Bank #%d - R $%02x - E $%02x\n",
-				//i, reg, bank_val[ i ]);
-			bad_expansion++;
+			//printf("ERROR BANK - R $%02x - E $AA\n", reg);
+			bad_main++;
 		}
 		else
 		{
-			// Now we can read data from original bank
-			poke(portb, 0x72);
+			// Now we can read data from expanded bank with NO BASIC
+			reg = 128 + mode_130xe[i];			// Select BANK #
+			reg = reg | (1 << 1);				// NO BASIC
+			reg = reg & 0xFE;					// OS ROM Ram space
+			poke(portb, reg);
 			reg = peek(wram);
 			CRAZY_COLOR;
-			// Test if it is the correct value 0xAA
-			if (reg != 0xAA)
-			{
-				//printf("ERROR Main - R $%02x - E $AA\n", reg);
-				bad_main++;
-			}
-			else
+			// Test if it is the correct value written
+			if (reg == bank_val[i])
 			{
 				// If it is correct, assume the bank is good!
 				count++;
 			}
+			else
+			{
+				//printf("ERROR Bank #%d - R $%02x - E $%02x\n",
+					//i, reg, bank_val[ i ]);
+				bad_expansion++;
+			}
 		}
 	}
+	EXIT_CRITICAL();
+	poke(portb, m_portb);
 	// 130xe mode = 128k + 64k
 	printf("%dK GOOD.\n", count * 16 + 64);
 	if (bad_main > 0)
@@ -184,66 +218,86 @@ int main(void)
 		printf("FAILED BANK: %d\n", bad_expansion); 
 #else
 	count = 0;
-	/* 576K mode: 576K RAM Pia Port B bank selection bit 1,2,3,5,6 */
+	// 576K mode: 576K RAM Pia Port B bank selection bit 1,2,3,5,6 (bit 4 must be 0)
 	printf("Checking  576K Mode: ");
+	ENTER_CRITICAL();
 	for (i = 0; i < 32; i++)
 	{
 		poke(portb, 0x72); // https://www.atarimagazines.com/v4n7/memorymanagement.html
-		/*
-		 * 7 6 5 4 3 2 1 0 
-		 * 0 1 1 1 0 0 1 0 == 0x72
-		 * 
-		 * CPU & ANTIC accessing Main Memory space (NO EXPANSION WINDOW)
-		 */
-		poke(wram, 0x55); // Watermark @ main bank 0x4000
-		CRAZY_COLOR;
-		
-		/* Now accessing @ bank # (CPU & ANTIC) */
-		poke(portb, 128 + mode_576k[ i ]);
-		poke(wram, bank_val[i]);    // Write i @ bank # @ 0x4000
+		//
+		// 7 6 5 4 3 2 1 0 
+		// 0 1 1 1 0 0 1 0 == 0x72
+		// 
+		// CPU & ANTIC accessing Main Memory space (NO EXPANSION WINDOW)
+		//
+		poke(wram, 0x55); // Watermark @ main bank
 		CRAZY_COLOR;
 
-		// read back the value
+		// Now accessing @ bank # (CPU & ANTIC)
+		reg = 128 + mode_576k[i];			// Select BANK #
+		reg = reg & 0xFE;					// OS ROM Ram space
+		// Basic bit is lost in this expansion
+		poke(portb, reg);
+		poke(wram, bank_val[i]);			// Write i @ bank # @
+		CRAZY_COLOR;
+	}
+
+	for (i = 0; i < 32; i++)
+	{
+		// Now read back the values
+		poke(portb, 0x72); // https://www.atarimagazines.com/v4n7/memorymanagement.html
+		//
+		// 7 6 5 4 3 2 1 0 
+		// 0 1 1 1 0 0 1 0 == 0x72
+		// 
+		// CPU & ANTIC accessing Main Memory space (NO EXPANSION WINDOW)
+		//
 		reg = peek(wram);
+		CRAZY_COLOR;
 		// Test if it is correctly written/read
-		if (reg != bank_val[ i ])
+		if (reg != 0x55)
 		{
-			//printf("ERROR Bank #%d - R $%02x - E $%02x\n",
-			//	i, reg, bank_val[ i ]);
-			bad_expansion++;
+			//printf("ERROR Main - R $%02x - E $AA\n", reg);
+			bad_main++;
 		}
 		else
 		{
-			// Now we can read data from original bank
-			poke(portb, 0x72);
+			// Now we can read data from expanded bank
+			reg = 128 + mode_576k[i];
+			reg = reg & 0xFE; // OS ROM Ram space
+			poke(portb, reg);
 			reg = peek(wram);
 			CRAZY_COLOR;
-			// Test if it is the correct value 0x55
-			if (reg != 0x55)
-			{
-				//printf("ERROR Main - R $%02x - E $55\n", reg);
-				bad_main++;
-			}
-			else
+			// Test if it is the correct value 
+			if (reg == bank_val[i])
 			{
 				// If it is correct, assume the bank is good!
 				count++;
 			}
+			else
+			{
+				//printf("ERROR Main #%d - R $%02x - E $%02x\n",
+				//	i, reg, bank_val[ i ]);
+				bad_expansion++;
+			}
 		}
 	}
+	EXIT_CRITICAL();
+	poke(portb, m_portb);
 	// 576k mode = 512k + 64k
 	printf("%dK GOOD.\n", count * 16 + 64);
 	if (bad_main > 0)
 		printf("FAILED MAIN: %d\n", bad_main); 
 	if (bad_expansion > 0)
-		printf("FAILED BANK: %d\n", bad_expansion); 
+		printf("FAILED BANK: %d\n", bad_expansion);
 #endif
 
+	poke(portb, peek(portb) | (1 << 4)); // disable expansion (bit 4 '1') ???
+
 	for (;;) {
-		if (peek(0xD01F) == 6)
-			break;
 		CRAZY_COLOR;
 	}
-//	c = cgetc();
+	// NEVERREACHED
+	c = cgetc();
 	return(0);
 }
